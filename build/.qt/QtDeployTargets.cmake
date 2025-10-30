b1d0221af9a3881300f5cf4646bb9140444d9d18
slzz <faceitff41@gmail.com>
2025-10-30 09:10:14 +0300

    Stop tracking build/ directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Scholar_FILE D:/cource_work/build/bin/Scholar.exe)
-set(__QT_DEPLOY_TARGET_Scholar_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Scholar_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
--- build/CMakeFiles/Scholar.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Scholar.
-# This may be replaced when dependencies are built.
